8c090fe02086a2dc7d182913af30854edab845f9
matkrin <matkrin@protonmail.com>
2025-03-24 10:16:05 +0100

    Move examples in examples folder

--- examples/example_for_specs_xy.py
+++ examples/example_for_specs_xy.py
@@ -2,19 +2,21 @@ from pathlib import Path
 from specs_xy_to_kolxpd import convert_specs_prodigy_xy
 import traceback
 
+
 def main():
-    data_path = Path('test-files')
-    out_path = Path('test-files')
-    xy_files = data_path.glob('*.xy')
+    data_path = Path("test-files")
+    out_path = Path("test-files")
+    xy_files = data_path.glob("*.xy")
     for file in xy_files:
         try:
             output = convert_specs_prodigy_xy(file)
-            name = file.name[:-3] + '.exp'
-            with open(out_path / name, 'w') as outfile:
+            name = file.name[:-3] + ".exp"
+            with open(out_path / name, "w") as outfile:
                 outfile.write(output)
         except Exception:
-            print(f'Failed to convert file {file.name}:')
+            print(f"Failed to convert file {file.name}:")
             print(traceback.format_exc())
+
 
 if __name__ == "__main__":
     main()
